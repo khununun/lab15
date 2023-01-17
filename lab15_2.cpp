a98ed3e594a38c7026c3cbfa8f537d9bdacd86da
khununun <khununun2546@gmail.com>
2023-01-17 17:01:53 +0700

    Done

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,51 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool A[][N])
+{
+    for(int a = 0; a < N; a++)
+    {
+        for(int b = 0; b < N; b++)
+        {
+          cout << A[a][b] << " ";
+        }
+    cout << endl;
+  } 
+}
+void inputMatrix(double A[][N])
+{
+    for(int i = 0; i < N;i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j = 0; j < N;j++){
+            cin >> A[i][j];
+        }
+    }
+}
+void findLocalMax(const double A[][N],bool B[][N])
+{
+    for(int i = 1;i < N-1;i++)
+    {
+        for(int j = 1;j < N-1;j++)
+        {
+            if(A[i][j]  >= A[i][j-1] && A[i][j]  >= A[i][j+1] && A[i][j]  >= A[i-1][j] && A[i][j]  >= A[i+1][j])
+            {
+                B[i][j] = 1;
+            }
+            else
+            {
+                B[i][j] = 0;
+            }
+        }
+    }
+    for(int x = 0; x < N;x++)
+    {
+        B[x][0] = 0;
+        B[x][N-1] = 0;
+        for(int y = 0;y < N;y++)
+        {
+            B[0][y] = 0;
+            B[N-1][y] = 0;
+        }
+    }
+    
+}
